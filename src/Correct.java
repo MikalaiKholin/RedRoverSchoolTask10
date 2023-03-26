8217e48dda5493b1c1cf94e990a5a313e0534a15
MikalaiKholin <48877386+MikalaiKholin@users.noreply.github.com>
2023-03-26 18:40:38 +0300

    feat : task10.1 complite

---
+++ src/Correct.java
@@ -0,0 +1,8 @@
+public class Correct {
+    public static String correct(String string) {
+        string = string.replaceAll("5", "S");
+        string = string.replaceAll("0", "O");
+        string = string.replaceAll("1", "I");
+        return string;
+    }
+}
