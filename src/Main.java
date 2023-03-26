c596d97bec4e2e7edc4bd174525e3c83772ffab2
MikalaiKholin <48877386+MikalaiKholin@users.noreply.github.com>
2023-03-26 19:19:20 +0300

    feat : task10.3 complite

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(BasicOperations.basicMath("*",2 , 2));
         System.out.println(BasicOperations.basicMath("/",9 , 0));
+        System.out.println(Arrays.toString(Sequence.reverse(10)));
     }
 }
