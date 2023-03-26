d48c1a173840eee1e45caabfaff8c7fc901966d6
MikalaiKholin <48877386+MikalaiKholin@users.noreply.github.com>
2023-03-26 19:20:06 +0300

    feat : task10.3 complite

--- src/Sequence.java
+++ src/Sequence.java
@@ -1,7 +1,7 @@
 public class Sequence {
     public static int[] reverse(int n){
         if (n <= 0) return new int[] {0};
-        int arr [] = new int[n];
+        int [] arr = new int[n];
         for (int i = n; i > 0; i--) arr[i - 1] = n - i + 1;
         return arr;
     }
